2300e6f3e0c6eba2e0ad16ed5eefcf1b95bd6cbb
ramimgaidi <80489089+ramimgaidi@users.noreply.github.com>
2021-11-10 22:47:36 +0100

    Add files via upload
    this is my work at front end :)

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.img-btn').addEventListener('click', function()
+	{ 
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+);
+/* La méthode querySelector() de l'interface Document retourne le premier Element dans le
+ document correspondant au sélecteur */ 
+ /*The classList property returns the class name(s) of an element*/
+ /* toggle = basculer */
