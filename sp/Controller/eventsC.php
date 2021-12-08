d0b14f4cf5193154e3ff0db8e0e7bb7e517b12b4
yahyasamet <73041526+yahyasamet@users.noreply.github.com>
2021-12-09 00:12:02 +0100

    gestion_events
    gestion events sponsors ticktes reactions

---
+++ sp/Controller/eventsC.php
@@ -0,0 +1,136 @@
+<?php
+	include 'C:/xampp/htdocs/sp/config2.php';
+	include_once 'C:/xampp/htdocs/sp/Model/events.php';
+	class eventsC {
+		function afficherevents(){
+			$sql="SELECT * FROM events";
+			$db = config2::getConnexion();
+			try{
+				$liste = $db->query($sql);
+				return $liste;
+			}
+			catch(Exception $e){
+				die('Erreur:'. $e->getMeesage());
+			}
+		}
+		function supprimerevents($id){
+			$sql="DELETE FROM events WHERE id_ev=:id_ev";
+			$db = config2::getConnexion();
+			$req=$db->prepare($sql);
+			$req->bindValue(':id_ev', $id);
+			try{
+				$req->execute();
+			}
+			catch(Exception $e){
+				die('Erreur:'. $e->getMeesage());
+			}
+		}
+		function ajouterevents($events){
+			$sql="INSERT INTO events (id_ev,nom_ev,lieu,descrip_lieu,date_ev,date_fin,nb_p,descrip_ev,spot) 
+			VALUES (:id_ev,:nom_ev,:lieu,:descrip_lieu,:date_ev,:date_fin,:nb_p,:descrip_ev,:spot)";
+			$db = config2::getConnexion();
+			try{
+				$query = $db->prepare($sql);
+				$query->execute([
+					'id_ev' => $events->getid(),
+					'nom_ev' => $events->getNom(),
+					'lieu' => $events->getlieu(),
+					'descrip_lieu' => $events->getdescrip_lieu(),
+                    'date_ev'=> $events-> getdate(),
+                    'date_fin'=> $events->getdate_fin(),
+                    'nb_p'=> $events->getnb_p(),
+					'descrip_ev'=> $events->getdescrip_ev(),
+					'spot'=>$events->getspot()
+
+				]);			
+			}
+			catch (Exception $e){
+				echo 'Erreur: '.$e->getMessage();
+			}			
+		}
+		function recupererevents($id){
+			$sql="SELECT * from events where id_ev=$id";
+			$db = config2::getConnexion();
+			try{
+				$query=$db->prepare($sql);
+				$query->execute();
+
+				$events=$query->fetch();
+				return $events;
+			}
+			catch (Exception $e){
+				die('Erreur: '.$e->getMessage());
+			}
+		}
+		
+		function modifierevents($events, $id){
+			try {
+				$db = config2::getConnexion();
+				$query = $db->prepare(
+					'UPDATE events SET 
+						id_ev= :id_ev,
+						nom_ev= :nom_ev,
+						lieu= :lieu,
+						descrip_lieu= :descrip_lieu,
+						date_ev= :date_ev,
+						date_fin= :date_fin,
+						nb_p= :nb_p,
+						descrip_ev= :descrip_ev,
+						spot= :spot
+						
+					WHERE id_ev= :id_ev'
+				);
+				$query->execute([
+					
+					'id_ev' =>$events->getid(),
+					'nom_ev' => $events->getNom(),
+					'lieu' => $events->getlieu(),
+					'descrip_lieu' => $events->getdescrip_lieu(),
+                    'date_ev'=> $events-> getdate(),
+                    'date_fin'=> $events->getdate_fin(),
+                    'nb_p'=> $events->getnb_p(),
+					'descrip_ev'=>$events->getdescrip_ev(),
+					'spot'=>$events->getspot()
+					
+					
+
+					
+				]);
+				echo $query->rowCount() . " records UPDATED successfully <br>";
+			} catch (PDOException $e) {
+				$e->getMessage();
+			}
+		}
+
+		function triereventsDESC()
+		{
+		$sql = "SELECT * from events ORDER BY date_ev DESC";
+		$db = config2::getConnexion();
+		try {
+		$req = $db->query($sql);
+		return $req;
+		} 
+		catch (Exception $e)
+	 	{
+		die('Erreur: ' . $e->getMessage());
+		}
+		}
+
+
+		function triereventsASC()
+		{
+		$sql = "SELECT * from events ORDER BY date_ev ASC";
+		$db = config2::getConnexion();
+		try {
+		$req = $db->query($sql);
+		return $req;
+		} 
+		catch (Exception $e)
+	 	{
+		die('Erreur: ' . $e->getMessage());
+		}
+	    }
+
+
+	}
+?>
---
+++ sp/Model/sponsors.php
@@ -0,0 +1,90 @@
+<?php
+	class sponsors{
+		private $id=null;
+		private $nom=null;
+		private $type=null;
+		private $numero=null;
+        private $email=null;
+		private $inves=null;
+		private $image=null;
+		private $descrip=null;
+		private $id2_ev=null;
+		function __construct($id, $nom,$type, $numero ,$email, $inves, $image,$descrip,$id2_ev){
+            $this->id=$id;
+			$this->nom=$nom;
+			$this->type=$type;
+			$this->numero=$numero;
+			$this->email=$email;
+			$this->inves=$inves;
+			$this->image=$image;
+			$this->descrip=$descrip;
+			$this->id2_ev=$id2_ev;
+			
+		}
+        // function get(var p)
+        // {
+            
+        //     return p;
+        // }
+
+		function getid(){
+			return $this->id;
+		}
+		function getNom(){
+			return $this->nom;
+		}
+		function getEmail(){
+			return $this->email;
+		}
+		function gettype(){
+			return $this->type;
+		}
+		function getnumero(){
+			return $this->numero;
+		}
+		function getinves(){
+			return $this->inves;
+		}
+		function getimage(){
+			return $this->image;
+		}
+
+		function getdescrip(){
+			return $this->descrip;
+		}
+		function getid_ev(){
+			return $this->id2_ev;
+		}
+		
+
+
+		function setNom(string $nom){
+			$this->nom=$nom;
+		}
+		function setEmail(string $email){
+			$this->email=$email;
+		}
+		function setnumero(string $numero){
+			$this->numero=$numero;
+		}
+		function settype(string $type){
+			$this->type=$type;
+		}
+		function setinves(string $inves){
+			$this->inves=$inves;
+		}
+		function setimage(string $image){
+			$this->image=$image;
+		}
+	
+		function setdescrip(string $descrip){
+			$this->descrip=$descrip;
+		}
+		function setid_ev(string $id2_ev){
+			$this->id2_ev=$id2_ev;
+		}
+		
+	}
+
+
+?>
